fad1bb0499ac245b993a29370faf39c215ed037d
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-28 23:58:52 +0300

    added: iris flower data basics

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 # graphing the iris flower dataset
 # and uncovering the patterns with machine learning algorithms
 
-import sklearn
-from sklearn import datasets
+import pandas as pd
+from sklearn.datasets import load_iris
 
+# preparing the data
+iris = load_iris()
+iris_data = pd.DataFrame(data = iris.data, columns = iris.feature_names)
+# add target labels
+iris_data['target'] = iris.target
+
+print(iris_data)
