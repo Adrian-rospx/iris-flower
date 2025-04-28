d91e71d9c0a76347fe676f58a29c97857dc5ce42
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-29 00:23:26 +0300

    added: visualisation with matplotlib and seaborn

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import pandas as pd
 from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # preparing the data
 iris = load_iris()
@@ -13,5 +15,12 @@ iris_data['target'] = iris.target
 # change target numbers to flower names
 iris_data['target_name'] = iris_data['target'].apply(lambda x: iris.target_names[x])
 
-print(iris_data)
-print(iris_data.head())
+# use seaborn for display and visualisation
+sns.pairplot(iris_data, hue = "target_name")
+plt.show()
+
+iris_data.hist(figsize=(10,8))
+plt.show()
+
+sns.boxplot(x="target_name", y="petal length (cm)", data=iris_data)
+plt.show()
