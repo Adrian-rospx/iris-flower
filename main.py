f35cf3152ac81884679b2aa9b68a47289239af14
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-29 00:05:09 +0300

    update: correct labeling

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ iris = load_iris()
 iris_data = pd.DataFrame(data = iris.data, columns = iris.feature_names)
 # add target labels
 iris_data['target'] = iris.target
+# change target numbers to flower names
+iris_data['target_name'] = iris_data['target'].apply(lambda x: iris.target_names[x])
 
 print(iris_data)
+print(iris_data.head())
